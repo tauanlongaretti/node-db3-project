c9bd297a7145630b493e647a93a7d48663b5cbab
tauan-longaretti <tauanlongaretti@gmail.com>
2020-04-16 00:30:06 -0400

    CamelCased statements for readability

--- queries.sql
+++ queries.sql
@@ -2,33 +2,33 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
-select productname, categoryname
-from product
-join category
-on product.categoryid = category.id
+SELECT ProductName, CategoryName
+FROM Product
+JOIN Category
+ON Product.CategoryId = Category.Id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
 
-select id, companyname
-from [order]
-join shipper
-on [order].shipvia = shipper.id
-where [order].orderdate < "2012-08-09"
+SELECT Id, CompanyName
+FROM [Order]
+JOIN Shipper
+ON [Order].ShipVia = Shipper.Id
+WHERE [Order].OrderDate < "2012-08-09"
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
-select productname, quantity 
-from orderdetail
-join product
-on product.id = orderdetail.productid
-where orderdetail.orderid = "10251"
-order by [product].productname
+SELECT ProductName, Quantity 
+FROM OrderDetail
+JOIN Product
+ON Product.Id = OrderDetail.ProductId
+WHERE OrderDetail.OrderId = "10251"
+ORDER BY [Product].ProductName
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 
-select [Order].id as "Order Id", CompanyName as "Customer's Company Name", Employee.LastName as "Employee's Last Name"
-from [Order] 
-join Customer
-on [Order].CustomerId = Customer.Id
-join Employee
-on [Order].EmployeeId = Employee.id
+SELECT [Order].Id as "Order Id", CompanyName as "Customer's Company Name", Employee.LastName as "Employee's Last Name"
+FROM [Order] 
+JOIN Customer
+ON [Order].CustomerId = Customer.Id
+JOIN Employee
+ON [Order].EmployeeId = Employee.Id
